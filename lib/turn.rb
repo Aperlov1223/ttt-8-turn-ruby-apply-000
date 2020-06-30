81b5784a3654d0726aa0c28dddd4b907e9c1f7ba
Alice Perlov <aperlov23@gmail.com>
2020-06-30 23:35:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,6 +28,18 @@ def move(board, index, first_player = "X")
   board[index] = first_player
 end
 
+def turn_count(array)
+  counter = 0
+  array.each do |element|
+    if (element == "X" || element == "O")
+      counter += 1
+    else
+      # do nothing
+    end
+  end
+  return counter
+end
+
 def current_player(board)
   if (turn_count(board).even? == true)
     return "X"
